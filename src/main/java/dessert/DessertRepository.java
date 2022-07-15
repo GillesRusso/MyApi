c7dbf90c1c506bef1349bd6ba674bedbd7e67a70
Gilles <gilles.russo@kbw.stud.ch>
2022-07-15 09:19:45 +0200

    Recipe Repo erweitert

--- src/main/java/dessert/DessertRepository.java
+++ src/main/java/dessert/DessertRepository.java
@@ -3,5 +3,5 @@ package dessert;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DessertRepository extends JpaRepository<Dessert, Long> {
-    Dessert findyByDessert (String name);
+    Dessert findyByDessertName (String name);
 }
--- src/main/java/ingredient/IngredientRepository.java
+++ src/main/java/ingredient/IngredientRepository.java
@@ -1,4 +1,8 @@
 package ingredient;
 
-public class IngredientRepository {
+import dessert.Dessert;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+    Ingredient findyByIngredientName (String name);
 }
