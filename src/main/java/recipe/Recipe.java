45f66a75b5308ebbb6849f976fa992d8b7306207
Gilles <gilles.russo@kbw.stud.ch>
2022-07-15 09:00:16 +0200

    Recipe-Klasse erweitert

--- src/main/java/recipe/Recipe.java
+++ src/main/java/recipe/Recipe.java
@@ -1,4 +1,70 @@
 package recipe;
 
+import dessert.Dessert;
+
+import javax.persistence.*;
+
 public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int stepNum;
+
+    private String name;
+
+    @ManyToMany
+    @JoinColumn(name = "dessert_id", referencedColumnName = "id")
+    private Dessert dessert;
+
+    public Recipe(){
+        super();
+    }
+
+    public Recipe(int stepNum, String name) {
+        this.stepNum = stepNum;
+        this.name = name;
+    }
+
+    public Dessert getDessert() {
+        return dessert;
+    }
+
+    public void setDessert(Dessert dessert) {
+        this.dessert = dessert;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getStepNum() {
+        return stepNum;
+    }
+
+    public void setStepNum(int stepNum) {
+        this.stepNum = stepNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "id=" + id +
+                ", stepNum=" + stepNum +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
