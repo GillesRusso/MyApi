5af8ecff365e1b8ae528aad0900e0ec880d612c1
Gilles <gilles.russo@kbw.stud.ch>
2022-07-15 14:30:09 +0200

    neue Methode hinzugefügt

--- src/main/java/ch/clip/samples/authapi/dessert/DessertController.java
+++ src/main/java/ch/clip/samples/authapi/dessert/DessertController.java
@@ -1,9 +1,15 @@
 package ch.clip.samples.authapi.dessert;
 
+import ch.clip.samples.authapi.user.AppUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static java.util.Collections.emptyList;
 
 @RestController
 @RequestMapping("/desserts")
@@ -16,9 +22,15 @@ public class DessertController {
         this.dessertRepository = dessertRepository;
     }
     @PostMapping("/{id}")
-    public void addDessert(@RequestBody Dessert name, @PathVariable Long id) {
-
+    public void addDessert(@RequestBody Dessert name) {
+        dessertRepository.save(name);
     }
+
+    public Dessert getDessert (@RequestBody Dessert name) {
+        Dessert dessert = dessertRepository.save(name);
+        return dessert;
+    }
+
     /**
      *
      * @return
--- src/main/java/ch/clip/samples/authapi/dessert/DessertRepository.java
+++ src/main/java/ch/clip/samples/authapi/dessert/DessertRepository.java
@@ -1,10 +1,8 @@
 package ch.clip.samples.authapi.dessert;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
 public interface DessertRepository extends JpaRepository<Dessert, Long> {
-  //Dessert FindyByDessertName (String name);
-
+    public Dessert findByName(Dessert name);
 }
